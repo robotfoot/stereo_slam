7ab8baf9809eea52b5b64f1a1db74e1b1b473409
will <wmacaluso@gmail.com>
2017-07-28 12:30:58 -0400

    update calibration

--- calibrate_stereo_cameras.sh
+++ calibrate_stereo_cameras.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-rosrun camera_calibration cameracalibrator.py --size 9x7 --square 0.019 right:=/stereo/right/image_rect left:=/stereo/left/image_rect --approximate=0.10 --no-service-check
+rosrun camera_calibration cameracalibrator.py --size 8x6 --square 0.019 right:=/stereo/right/image_rect left:=/stereo/left/image_rect --approximate=0.10 --no-service-check
