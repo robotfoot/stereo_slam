5995b9819ec4926e5d82b21ca40cc722109b34ae
will <wmacaluso@gmail.com>
2017-07-28 10:31:50 -0400

    init

---
+++ catkin_ws/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/will/stereo_slam/catkin_ws/devel:$CMAKE_PREFIX_PATH"
+export PWD="/home/will/stereo_slam/catkin_ws/build"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/will/stereo_slam/catkin_ws/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/will/stereo_slam/catkin_ws/src:$ROS_PACKAGE_PATH"
